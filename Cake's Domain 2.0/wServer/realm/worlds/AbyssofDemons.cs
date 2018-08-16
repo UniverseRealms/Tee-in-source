35db6c9ed8afbccc6355f4a0241928015a861bc5
Tidan <raphealsmall@gmail.com>
2018-08-15 21:16:52 -0500

    Added Source And Client
    Description

---
+++ Cake's Domain 2.0/wServer/realm/worlds/AbyssofDemons.cs
@@ -0,0 +1,29 @@
+﻿#region
+
+using wServer.networking;
+
+#endregion
+
+namespace wServer.realm.worlds
+{
+    public class AbyssofDemons : World
+    {
+        public AbyssofDemons()
+        {
+            Name = "Abyss of Demons";
+            ClientWorldName = "Abyss of Demons";
+            Background = 0;
+            AllowTeleport = true;
+        }
+
+        protected override void Init()
+        {
+            LoadMap("wServer.realm.worlds.maps.HauntedCemeteryFinalBattle.jm", MapType.Json);
+        }
+
+        public override World GetInstance(Client psr)
+        {
+            return Manager.AddWorld(new AbyssofDemons());
+        }
+    }
+}
